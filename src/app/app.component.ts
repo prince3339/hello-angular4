3589a92673d0cd1b55ecb5c7687e72b55dafddcb
Prince <prince3339@ymail.com>
2017-09-18 16:22:29 +0600

    event emitter added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  rootName = 'Sabbir Ahmed';
+
+  onNameChanged(newName) {
+    this.rootName = newName;
+  }
 }
--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core'; 
+import { Component, Input, Output, EventEmitter } from '@angular/core'; 
 
 @Component({ 
   selector: 'app-user',
   template: `
-      <!-- <input type="text" (input) = "onUserInput($event)" [value] = "name"> -->
-      <input type="text" [(ngModel)] = "name">
+      <input type="text" (input) = "onUserInput($event)" [value] = "name">
+      <!-- <input type="text" [(ngModel)] = "name"> -->
       <p>Hello {{ name }}</p>
     `
   })
 
 export class UserComponent {
-  name = 'Prince';
+  @Input() name = 'Prince';
+  @Output() nameChanged = new EventEmitter <String>();
 
   onUserInput(event) {
     this.name = event.target.value;
+    this.nameChanged.emit(event.target.value);
   }
 }
